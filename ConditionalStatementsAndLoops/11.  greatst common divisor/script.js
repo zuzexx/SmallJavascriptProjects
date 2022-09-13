bb7f7a524dfc532d09a4f291376db9d5c736ca3b
zuzexx <tjasa.zilavec@gmail.com>
2022-09-13 16:21:24 +0200

    .

--- ConditionalStatementsAndLoops/11.  greatst common divisor/script.js
+++ ConditionalStatementsAndLoops/11.  greatst common divisor/script.js
@@ -1,19 +1,16 @@
-let text = document.getElementById("text");
+let text = "";
 
 let a = 1234;
 let b = 100;
 let gcd;
 
-while (a!=b)
-{
-	if (a>b)
-	{
-		a = a -b;
-	}
-	else
-	{
-		b = b - a;
-	}
+while (a != b) {
+  if (a > b) {
+    a = a - b;
+  } else {
+    b = b - a;
+  }
 }
 gcd = a;
-text.textContent=`greatest comon divider is ${gcd}`
+text = `greatest comon divider is ${gcd}`;
+console.log(text);
--- ConditionalStatementsAndLoops/12. sum of multiples of 5 and 3/script.js
+++ ConditionalStatementsAndLoops/12. sum of multiples of 5 and 3/script.js
@@ -1,9 +1,11 @@
-let text = document.getElementById("text");
+let text = "";
 let sum = 0;
 
-for (let i = 0; i<1000; i++){
-    if(i%3===0 || i%5===0){
-        sum += i;
-    }
+for (let i = 0; i < 1000; i++) {
+  if (i % 3 === 0 || i % 5 === 0) {
+    sum += i;
+  }
 }
-text.textContent = `the sum of multples of 3 and 5 is ${sum}`
+text = `the sum of multples of 3 and 5 is ${sum}`;
+
+console.log(text);
